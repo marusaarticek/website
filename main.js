3c6e45177594cb699c0f8195ddcc8463d7ea1173
unknown <articek.marusa@gmail.com>
2022-03-10 11:58:21 +0100

    updating

--- main.js
+++ main.js
@@ -19,17 +19,15 @@ camera.position.setZ(30); //default position is center
 
 renderer.render(scene, camera); // render == draw
 
+//-----------------------------------------------
 
-
-
-const geometry = new THREE.SphereGeometry( 10, 10, 10 );
-const material = new THREE.MeshStandardMaterial( { color: 0xFF6347} );
+const geometry = new THREE.SphereGeometry( 1, 1, 1 );
+const material = new THREE.MeshStandardMaterial( { color: 0xF5F5F5} );
 const sphere = new THREE.Mesh( geometry, material );
 
 sphere.position.x += 5;
 sphere.position.y += 5;
 sphere.position.z += 15;
-
 
 scene.add( sphere );
 
@@ -45,15 +43,16 @@ const lightHelper = new THREE.PointLightHelper(pointLight)
 //const gridHelper = new THREE.GridHelper(200, 50);
 scene.add(lightHelper)
 
-// orbit controls: move around the scene
+// orbit controls: move around the scene, listen to dom events on the mouse
 const controls = new OrbitControls(camera, renderer.domElement);
 
 //  adding background image 
-const backwall = new THREE.TextureLoader().load('soft.jpg');
+const backwall = new THREE.TextureLoader().load('pastelorange.jpg');
 scene.background = backwall;
 
 const loader = new GLTFLoader();
 
+/*
 loader.load( 'bear.glb', function ( gltf ) {
 
 	scene.add( gltf.scene );
@@ -63,14 +62,47 @@ loader.load( 'bear.glb', function ( gltf ) {
 	console.error( error );
 
 } );
+*/
+
+// Adding 3D glf files:
+// load glb file, generate random x, y, z position for each mesh
+// add number of meshes with the array
+
+function addBear() {
+  loader.load( 'bear.glb', function ( gltf ) {
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(250) );
+    const bear = gltf.scene;
+    bear.position.set(x, y, z);
+    scene.add( gltf.scene );
+  }, undefined, function ( error ) {
+    console.error( error );
+  } );
+}
+
+Array(200).fill().forEach(addBear)
+
+//-------------------------
+
+
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  
+  camera.position.z = t * 0.01;
+  camera.position.x = t * 0.01;
+  camera.rotation.y = t * 0.0001;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
 
 
 function animate() {
   requestAnimationFrame( animate );
 
   sphere.rotation.x += 0.01;
-  sphere.rotation.y += 0.005;
-  sphere.rotation.z += 0.01;
+  sphere.rotation.y += 0.02;
+  sphere.rotation.z += 0.02;
 
   controls.update();
 
